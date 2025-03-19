4de745f6d490cd6e3d106748f98fd6dc08431a83
jeevanandham4203 <jeevajee4203@gmail.com>
2025-03-19 12:03:00 +0530

    first Commited Successfully

---
+++ src/main/java/com/employee/CRUD/Entity/Employee.java
@@ -0,0 +1,28 @@
+package com.employee.CRUD.Entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name="emp_details")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Employee {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String name;
+	private int age;
+	private String gender;
+	private int salary;
+	private String role;
+}
